733b0fda29d4b579aad4f66e3ad357ddbb4dcb4b
usersayan <65238988+usersayan@users.noreply.github.com>
2020-05-12 21:09:40 +0530

    Add files via upload

---
+++ Barcode/Barcode/Barcode.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace Barcode
+{
+    partial class Barcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel25 = new System.Windows.Forms.Panel();
+            this.label38 = new System.Windows.Forms.Label();
+            this.comboBox14 = new System.Windows.Forms.ComboBox();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chk = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column28 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column29 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column30 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel25.SuspendLayout();
+            this.panel19.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Controls.Add(this.comboBox3);
+            this.panel5.Controls.Add(this.button3);
+            this.panel5.Location = new System.Drawing.Point(507, 228);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(290, 71);
+            this.panel5.TabIndex = 250;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 176;
+            this.label2.Text = "*Paper Size:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Label"});
+            this.comboBox3.Location = new System.Drawing.Point(222, 25);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(63, 21);
+            this.comboBox3.TabIndex = 176;
+            this.comboBox3.Text = "Label";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(9, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 40);
+            this.button3.TabIndex = 244;
+            this.button3.Text = "Print Barcode";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.comboBox2);
+            this.panel4.Location = new System.Drawing.Point(507, 305);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(290, 46);
+            this.panel4.TabIndex = 249;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(22, 7);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(241, 21);
+            this.comboBox2.TabIndex = 175;
+            // 
+            // panel25
+            // 
+            this.panel25.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel25.Controls.Add(this.label38);
+            this.panel25.Controls.Add(this.comboBox14);
+            this.panel25.Location = new System.Drawing.Point(507, 177);
+            this.panel25.Name = "panel25";
+            this.panel25.Size = new System.Drawing.Size(290, 46);
+            this.panel25.TabIndex = 248;
+            // 
+            // label38
+            // 
+            this.label38.AutoSize = true;
+            this.label38.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label38.Location = new System.Drawing.Point(6, 12);
+            this.label38.Name = "label38";
+            this.label38.Size = new System.Drawing.Size(90, 17);
+            this.label38.TabIndex = 163;
+            this.label38.Text = "*Type / Model:";
+            // 
+            // comboBox14
+            // 
+            this.comboBox14.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox14.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox14.FormattingEnabled = true;
+            this.comboBox14.Location = new System.Drawing.Point(99, 11);
+            this.comboBox14.Name = "comboBox14";
+            this.comboBox14.Size = new System.Drawing.Size(153, 21);
+            this.comboBox14.TabIndex = 175;
+            this.comboBox14.SelectedIndexChanged += new System.EventHandler(this.comboBox14_SelectedIndexChanged);
+            // 
+            // panel19
+            // 
+            this.panel19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel19.Controls.Add(this.comboBox1);
+            this.panel19.Controls.Add(this.label1);
+            this.panel19.Controls.Add(this.label26);
+            this.panel19.Controls.Add(this.comboBox8);
+            this.panel19.Location = new System.Drawing.Point(507, 76);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(290, 95);
+            this.panel19.TabIndex = 247;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(78, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.TabIndex = 177;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 176;
+            this.label1.Text = "*Product :";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label26.Location = new System.Drawing.Point(6, 60);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(52, 17);
+            this.label26.TabIndex = 163;
+            this.label26.Text = "*Brand :";
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox8.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(78, 60);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(178, 21);
+            this.comboBox8.TabIndex = 175;
+            this.comboBox8.SelectedIndexChanged += new System.EventHandler(this.comboBox8_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(-288, -250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 252;
+            this.checkBox1.Text = "SELECT ALLL";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chk,
+            this.Column1,
+            this.Column2,
+            this.Column28,
+            this.Column29,
+            this.Column30,
+            this.Column3,
+            this.Column8});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(478, 330);
+            this.dataGridView1.TabIndex = 251;
+            // 
+            // chk
+            // 
+            this.chk.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.chk.HeaderText = "";
+            this.chk.Name = "chk";
+            this.chk.Width = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "brand";
+            this.Column1.HeaderText = "BRAND";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "product";
+            this.Column2.HeaderText = "PRODUCT";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 76;
+            // 
+            // Column28
+            // 
+            this.Column28.DataPropertyName = "types";
+            this.Column28.HeaderText = "TYPE / MODEL";
+            this.Column28.Name = "Column28";
+            // 
+            // Column29
+            // 
+            this.Column29.DataPropertyName = "description";
+            this.Column29.HeaderText = "DESCRIPTION";
+            this.Column29.Name = "Column29";
+            this.Column29.Visible = false;
+            // 
+            // Column30
+            // 
+            this.Column30.DataPropertyName = "barcode";
+            this.Column30.HeaderText = "BARCODE";
+            this.Column30.Name = "Column30";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "qty";
+            this.Column3.HeaderText = "QTY";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 110;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "saleprice";
+            this.Column8.HeaderText = "S.P.";
+            this.Column8.Name = "Column8";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(848, 30);
+            this.panel2.TabIndex = 254;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(280, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(254, 22);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "BARCODE GENEREATION";
+            // 
+            // Barcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 447);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel25);
+            this.Controls.Add(this.panel19);
+            this.Name = "Barcode";
+            this.Text = "Barcode";
+            this.Load += new System.EventHandler(this.Barcode_Load);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel25.ResumeLayout(false);
+            this.panel25.PerformLayout();
+            this.panel19.ResumeLayout(false);
+            this.panel19.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel5;
+        internal System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel25;
+        internal System.Windows.Forms.Label label38;
+        private System.Windows.Forms.ComboBox comboBox14;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.ComboBox comboBox1;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label label26;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column28;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column29;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column30;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+    }
+}
+
---
+++ Barcode/Barcode/Barcode.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Reflection;
+
+namespace Barcode
+{
+    public partial class Barcode : Form
+    {
+        public Barcode()
+        {
+            InitializeComponent();
+        }
+        string ss = ConfigurationManager.ConnectionStrings["kkk"].ConnectionString;
+
+   
+        public void GridCounterStock()
+        {
+            SqlConnection con = new SqlConnection(ss);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select Qty from counter_stock ", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                SqlConnection con1 = new SqlConnection(ss);
+                con1.Open();
+                SqlCommand cmd1 = new SqlCommand("delete from counter_stock where Qty<=0 ", con1);
+                cmd1.ExecuteNonQuery();
+                con1.Close();
+
+                SqlConnection con2 = new SqlConnection(ss);
+                con2.Open();
+                SqlDataAdapter da2 = new SqlDataAdapter("exec counterbarcode ", con2);
+                DataSet ds2 = new DataSet();
+                da2.Fill(ds2, "counter_stock");
+                dataGridView1.DataSource = ds2.Tables["counter_stock"];
+                con2.Close();
+            }
+            else
+            {
+                SqlConnection con3 = new SqlConnection(ss);
+                con3.Open();
+                SqlDataAdapter da3 = new SqlDataAdapter(" exec counterbarcode", con3);
+                DataSet ds3 = new DataSet();
+                da3.Fill(ds3, "counter_stock");
+                dataGridView1.DataSource = ds3.Tables["counter_stock"];
+                con3.Close();
+            }
+        }
+
+        private void Barcode_Load(object sender, EventArgs e)
+        {
+            SqlConnection con31 = new SqlConnection(ss);
+            con31.Open();
+            SqlDataAdapter da31 = new SqlDataAdapter("select distinct(product) from counter_stock ", con31);
+            DataSet ds31 = new DataSet();
+            da31.Fill(ds31, "counter_stock");
+            comboBox1.DataSource = ds31.Tables["counter_stock"];
+            comboBox1.DisplayMember = "product";
+            con31.Close();
+
+            SqlConnection con12 = new SqlConnection(ss);
+            con12.Open();
+            SqlDataAdapter da12 = new SqlDataAdapter("select distinct(counter) from counter_stock", con12);
+            DataSet ds12 = new DataSet();
+            da12.Fill(ds12, "counter_stock");
+            comboBox2.DataSource = ds12.Tables["counter_stock"];
+            comboBox2.DisplayMember = "counter";
+            con12.Close();
+
+            SqlConnection con3 = new SqlConnection(ss);
+            con3.Open();
+            SqlDataAdapter da3 = new SqlDataAdapter("select distinct(Types) from counter_stock", con3);
+            DataSet ds3 = new DataSet();
+            da3.Fill(ds3, "counter_stock");
+            comboBox14.DataSource = ds3.Tables["counter_stock"];
+            comboBox14.DisplayMember = "Types";
+            con3.Close();
+
+
+
+            GridCounterStock();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection con10 = new SqlConnection(ss);
+            con10.Open();
+            SqlDataAdapter da10 = new SqlDataAdapter("select distinct(Brand) from counter_stock where Product='" + comboBox1.Text + "'", con10);
+            DataSet ds10 = new DataSet();
+            da10.Fill(ds10, "counter_stock");
+            comboBox8.DataSource = ds10.Tables["counter_stock"];
+            comboBox8.DisplayMember = "Brand";
+            con10.Close();
+        }
+
+        private void comboBox8_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection con1 = new SqlConnection(ss);
+            con1.Open();
+            SqlDataAdapter da1 = new SqlDataAdapter("select product,brand,types,description,barcode,qty,saleprice from counter_stock  where product like '" + comboBox1.Text + "%'  and Brand like '" + comboBox8.Text + "%' order by Brand  ", con1);
+            DataSet ds1 = new DataSet();
+            da1.Fill(ds1, "counter_stock");
+            dataGridView1.DataSource = ds1.Tables["counter_stock"];
+            con1.Close();
+        }
+
+        private void comboBox14_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ss);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select product,brand,types,description,barcode,qty,saleprice from  counter_stock where Types like '" + comboBox14.Text + "%' order by Brand  ", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "counter_stock");
+            dataGridView1.DataSource = ds.Tables["counter_stock"];
+            con.Close();
+        }
+
+        public static string prod, brand, paper;
+        private void button3_Click(object sender, EventArgs e)
+        {
+              SqlConnection con11 = new SqlConnection(ss);
+                con11.Open();
+                SqlCommand cmd11 = new SqlCommand("truncate table barcode_counterstock ", con11);
+                cmd11.ExecuteNonQuery();
+                con11.Close();
+
+
+                if (dataGridView1.RowCount > 0)
+                {
+                    int inserted = 0;
+
+
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        bool isSelected = Convert.ToBoolean(row.Cells["chk"].Value);
+                        if (isSelected)
+                        {
+                            using (SqlConnection con = new SqlConnection(ss))
+                            {
+                                using (SqlCommand cmd = new SqlCommand("insert into barcode_counterstock values(@counter,@product,@brand,@types,@description,@barcode,@qty,@saleprice)", con))
+                                {
+
+                                    cmd.Parameters.AddWithValue("@counter", comboBox2.Text);
+                                    cmd.Parameters.AddWithValue("@product", row.Cells[1].Value);
+                                    cmd.Parameters.AddWithValue("@brand", row.Cells[2].Value);
+                                    cmd.Parameters.AddWithValue("@types", row.Cells[3].Value);
+                                    cmd.Parameters.AddWithValue("@description", row.Cells[4].Value);
+                                    cmd.Parameters.AddWithValue("@barcode", row.Cells[5].Value);
+                                    cmd.Parameters.AddWithValue("@qty", row.Cells[6].Value);
+                                    //cmd.Parameters.AddWithValue("@Unit", row.Cells[7].Value);
+                                    cmd.Parameters.AddWithValue("@saleprice", row.Cells[7].Value);
+                                   
+
+
+                                    con.Open();
+                                    cmd.ExecuteNonQuery();
+                                    con.Close();
+
+                                }
+                            }
+                            inserted++;
+                        }
+
+                    }
+
+                    if (inserted > 0)
+                    {
+                        MessageBox.Show(string.Format("{0} Barcode Printing.", inserted), "Message");
+
+                        prod = comboBox1.Text;
+                        brand = comboBox8.Text;
+
+                        Print_Barcode pb = new Print_Barcode();
+                        pb.Show();
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select Checkbox to Print Barcode");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No records found");
+                }
+            
+          
+        }
+
+   
+
+    }
+}
